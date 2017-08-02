2890025d07fddbe2c7e7dee045881a2cdeded80d
root <448215986@qq.com>
2017-08-02 11:14:30 +0800

    201708021114

---
+++ dirToJson.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+def thefileTree(mm):
+    a = {}
+    mm1 = os.listdir(mm)
+
+    for i in mm1:
+
+        print type(i)
+        print i
+        if i == '.idea' or i == '.git':
+            continue
+        if os.path.isdir(mm + i + '/'):
+            dirname = mm + i + '/'
+            a[dirname] = (thefileTree(dirname))
+        else:
+
+            a[i] = i
+
+    return a
+
+
+def writeToFile(mm):
+    jsonStr = json.dumps(mm)
+    print jsonStr
+    print mm
+    # f = open('dir.js', 'w')
+    # f.write("var a=" + jsonStr)
+xx = thefileTree('./')
+# print xx
+writeToFile(xx)
